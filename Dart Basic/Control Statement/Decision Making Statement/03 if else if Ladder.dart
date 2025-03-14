172bfbf42c4b35e22c865e92a20c9d7d8ef910c7
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-14 23:19:02 +0530

    Control Statement

---
+++ Dart Basic/Control Statement/Decision Making Statement/03 if else if Ladder.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  int No1 = 15;
+  int No2 = 40; 
+
+  if(No1 >= No2)
+  {
+    print("No1 is Maximum From No2");
+  }
+  else if(No1 <= No2)
+  {
+    print("No2 is Maximum From No1");
+  }
+  else
+  {
+    print("Both Numbers Are Equal");
+  }
+}
