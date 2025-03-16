3e711f1d4823241cd6fc65dd1e4d5663a21f55e4
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-16 23:01:37 +0530

    Control Statement

---
+++ Dart Basic/Control Statement/Decision Making Statement/04 Nested if else.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  int no = 40;
+
+  if(no == 40)
+  {
+    if(no <25)
+    {
+      print("No is Less Than 25");
+    }
+    else
+    {
+      print("No is Greater Than 25");
+    }
+  }
+  else
+  {
+    print("No is Not Equal To 40");
+  }
+}
