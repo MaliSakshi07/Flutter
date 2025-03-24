0efed3eddca9d80e0417986f312aa8001097e192
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-24 23:30:00 +0530

    Logical Operator

---
+++ Dart Basic/Operators/Logical/02 Logical - OR.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  int No1 = 10;
+  int No2 = -20;
+
+  if(No1 > 0 || No2 > 0)
+  {
+    print("Any One Of The Value is Greater Than Zero");
+  }
+  else
+  {
+    print("Both Values Are Less Than Zero");
+  }
+}
