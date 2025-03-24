bb3f1eaebcdcc2c7f4eb404d720d5d7276443201
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-24 23:28:12 +0530

    Logical Operator

---
+++ Dart Basic/Operators/Logical/01 Logical - AND.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  int No1 = 10;
+  int No2 = 20;
+
+  if(No1 > 0 && No2 > 0 )
+  {
+    print("Both Values Are Greater Than Zero");
+  }
+  else
+  {
+    print("Both Values Are Less Than Zero");
+  }
+}
