abea0e3670edb94c1c470c44f3e4d043024f6735
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-25 23:23:48 +0530

    Relational Operator

---
+++ Dart Basic/Operators/Relational/02 Minimum From Two Numbers.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  int No1 = 20;
+  int No2 = 40;
+
+  if(No1 <= No2)
+  {
+    print("First Number is Minimum From Second Number");
+  }
+  else
+  {
+    print("Second Number is Minimum From First Number");
+  }
+}
