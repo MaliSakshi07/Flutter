6723432960881ce0d21550231688e10d9e59e37f
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-25 23:22:56 +0530

    Relational Operator

---
+++ Dart Basic/Operators/Relational/01 Maximum From Two Numbers.dart
@@ -0,0 +1,15 @@
+void main()
+{
+  int No1 = 40;
+  int No2 = 10;
+
+  if(No1 >= No2)
+  {
+    print("First Number is Maximum From Second Number");
+  }
+  else 
+  {
+    print("Second Number is Maximum From First Number");
+  }
+
+}
