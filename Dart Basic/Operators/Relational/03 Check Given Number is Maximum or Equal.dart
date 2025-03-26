dd156f891558254cf946037cc21a293332c4d4cd
MaliSakshi07 <sakshimali467@gmail.com>
2025-03-26 23:23:38 +0530

    Relational Operator

---
+++ Dart Basic/Operators/Relational/03 Check Given Number is Maximum or Equal.dart
@@ -0,0 +1,19 @@
+
+void main()
+{
+  int No1 = 20;
+  int No2 = 50;
+
+  if(No1 == No2)
+  {
+    print("Both Numbers are Equal");
+  }
+  else if(No1 < No2)
+  {
+    print("First Number is Maximum");
+  }
+  else 
+  {
+    print("Second Number is Maximum");
+  }
+}
