7d83dc08c75215c9cde40423842b96f88a7fcb4f
MaliSakshi07 <sakshimali467@gmail.com>
2025-04-17 23:30:32 +0530

    Inheritance

---
+++ Dart Assignment/Assignment - 02/03 Hierarchical Inheritance.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+class Watch {
+  String shopName = "Just In Time";
+    String location = "Karad";
+
+    void displayShopInfo(){
+      stdout.writeln("Shop Name : ${shopName}");
+      stdout.writeln("Shop Location : ${location}");
+  }
+}
+class DialWatch extends Watch {
+    String? dBrand;
+    String? dModel;
+    num? dPrice;
+
+    void watchDetails(){
+        stdout.writeln("DialWatch Brand Name :");
+        dBrand = stdin.readLineSync()!;
+
+        stdout.writeln("DialWatch Model Name :");
+        dModel = stdin.readLineSync()!;
+
+        stdout.writeln("DialWatch Price :");
+        dPrice = num.parse(stdin.readLineSync()!);
+    }
+   
+    void displayWatchInfo(){
+        stdout.writeln("DialWatch Brand Name : ${dBrand}");
+        stdout.writeln("DialWatch Model Name : ${dModel}");
+        stdout.writeln("DialWatch Price : ${dPrice}");
+  }
+}
+// Derived class
+class SmartWatch extends Watch {
+    String? brand;
+    String? model;
+    double? price;
+
+    SmartWatch(this.brand,this.model, this.price);
+   
+    void displaySmartWatch(){
+        stdout.writeln("SmartWatch Brand Name : ${brand}");
+        stdout.writeln("SmartWatch Model Name : ${model}");
+        stdout.writeln("SmartWatch Price : ${price}");
+  }
+}
+
+// Main function
+void main() {
+  Watch sw = Watch("titen", "Analogue", 2000.00);
+  sw.watchDetails();
+  sw.displayShopInfo();
+  sw.displaySmartWatch();
+  sw.displayWatchInfo();
+}
