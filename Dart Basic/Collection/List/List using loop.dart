9dd62e47590a531107b6e2c112fb07bfb130cab0
MaliSakshi07 <sakshimali467@gmail.com>
2025-05-04 22:21:27 +0530

    Collection

---
+++ Dart Basic/Collection/List/List using loop.dart
@@ -0,0 +1,8 @@
+void main(){
+  List fruitName = ['Apple','Banana','Mango','Strawberry'];
+
+  fruitName.add('Orange');
+
+  for(int idx = 0; idx < fruitName.length; idx++)
+    print(fruitName[idx]);
+}
