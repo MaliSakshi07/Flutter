4f3ddb3b67ff04b2c2433e5aad395267c83c2ba0
MaliSakshi07 <sakshimali467@gmail.com>
2025-05-05 21:33:31 +0530

    Collection

---
+++ Dart Basic/Collection/List/01 List - Add Method.dart
@@ -0,0 +1,9 @@
+void main(){      ///Entry point function
+  List studentNameList = ['Sakshi','Om','Sanika'];
+
+  print(studentNameList);
+
+  studentNameList.add('Prachi');
+
+  print(studentNameList);
+}
