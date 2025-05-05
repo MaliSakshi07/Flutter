5867fcbf4c3e6457a9d1f5318541ad596214ce7f
MaliSakshi07 <sakshimali467@gmail.com>
2025-05-05 21:34:29 +0530

    Collection

---
+++ Dart Basic/Collection/List/02 List - Remove Method.dart
@@ -0,0 +1,9 @@
+void main(){        ///Entry point function
+  List studentNameList = ["Sakshi","Sanika","Anu"," Rani"];
+
+  print(studentNameList);
+
+  studentNameList.remove("Anu");
+
+  print(studentNameList);
+}
