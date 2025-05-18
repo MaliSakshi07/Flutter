407a8a0c05875f18071dfde4c6a5d953e1f5adf7
MaliSakshi07 <sakshimali467@gmail.com>
2025-05-18 23:33:06 +0530

    Assignment

---
+++ Flutter Assignments/Assignment - 05/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Assignment - 05"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.pink,
+          ),
+        ),
+      ),
+    );
+  }
+}
